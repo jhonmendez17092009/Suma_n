e189918baa38aeefab74c8fd87d82053475b748a
jhonmendez17092009 <jhoasnelmendez@colegioguanenta.edu.co>
2025-02-13 18:10:00 -0500

    segundo push

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Suma_n
+
+# Analisis
+
+
+## Input
+### Variables de entrada
+n: valor a ingresar 
+### processin
+
+s = s + i
+i = i + 1
+
+### output
+
+# Diseño
+![Diagrama de flugo](diagrama.png "Diagrama de flujo")
+
+# Construcion 
---
+++ suma_n.py
@@ -0,0 +1,13 @@
+# input
+n = int(input("digite el valor de n : 5"))
+
+# processing
+
+s = 0
+i = 1
+
+while i <= n :
+    s = s + i
+    i = i + 1
+
+print("la suma de los " + str(n) + "suma de los primeros  numeros naturales es " + str(s))
